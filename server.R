f132fe743a0d7d35ed22364559908cf80a8e8f0e
kchaaa <kevinjcha96@gmail.com>
2017-06-26 15:58:38 -0700

    sync with crpe comp

--- server.R
+++ server.R
@@ -1,44 +1,69 @@
+# Title: Shiny Demo App for CRPE Database [Server/Backend]
+# Owner: Kevin 
+# Date: 6-23-2016
+
+#### COMMENT THESE OUT BEFORE PUBLISHING ()
+# rm(list=ls())
+# setwd("/Users/crpe/Documents/Shiny Demo") # MAC
+
+library(shiny) # Need to Run Shiny App
+library(shinyjs) # Need to Make Fields Mandatory/Optional and Do Other Cool Stuff
+library(DBI) # Need to Query with Database
+library(plyr) # Optional (for now)
+library(dplyr) # Optional (for now)
+library(dbplyr) # Allows you to interact with the database using dplyr instead of SQL (same concept, different terms)
+# IF having problems, use devtools::install_github("tidyverse/dbplyr") (make sure to have devtools installed first)
+library(RPostgreSQL) # Need to Read PostgreSQL
 
 
-library(shiny)
-library(DBI)
-library(plyr)
-library(dplyr)
-library(dbplyr)
-library(RPostgreSQL)
+## Establish Connection to DB --------------------------------------------------------
+# Connects to the server
+conn <- 
+  dbConnect(
+    drv = dbDriver("PostgreSQL"), 
+    dbname = "Education",
+    host = 'education.csj8biafq77k.us-west-2.rds.amazonaws.com',
+    port = '5432',
+    user = "crpe",
+    password = "!crpecrpe1"
+  )
+## TEST: check if the connection is successful (MAKE SURE TO COMMENT OUT BEFORE RUNNING)
+# Run together with conn from above^
+# dbListTables(conn)
+# RESULT Should Be: "mockschools" "mockcensus", mockstates (or whatever the list of datasets are in the DB)
 
-# Define server logic required to draw a histogram
+
+## Backend of the Shiny App --------------------------------------------------------
 shinyServer(function(input, output) {
-  ## SidePanel ##
+  ## SidePanel ---------------------------------------------------------------------
   
   
   
-  
-  ## MainPanel ##
-  # connects to database
+  ## MainPanel ---------------------------------------------------------------------
+  # Renders the data table based on the inputs from the side panel
   output$my_table <- renderDataTable({
-    conn <- 
-      # Connects to the server
-      dbConnect(
-      drv = dbDriver("PostgreSQL"),
-      dbname = "Education",
-      host = 'education.csj8biafq77k.us-west-2.rds.amazonaws.com',
-      port = '5432',
-      user = "crpe",
-      password = "test12CRPE!"
-      )
-      # Exits 
-      on.exit(dbDisconnect(conn), add = TRUE)
-      # Gets the data
-      dbGetQuery(conn, paste0(
-        "SELECT * FROM mockschools ", input$dataset, ";"))
+    
+    
+    # Gets the Data
+    dbGetQuery(conn, paste0(
+      "SELECT * FROM ", input$dataset, ";"))
+    
+    # Disconnects from the Database Once User Done using App 
+    on.exit(dbDisconnect(conn), add = TRUE)
+    
   })
   
-  #download button
-  output$downloadData <- downloadHandler(
-    filename = function() { paste(input$dataset, '.csv', sep='') },
+  # Prepares Data to be Downloaded
+  datasetInput <- reactive({
+    
+  })
+  
+  # Download Button
+  output$downloadBttn <- downloadHandler(
+    filename = function() { paste0('test', '.csv') },
     content = function(file) {
-      write.csv(datasetInput(), file)
+      write.csv(datasetInput(), file, row.names = FALSE)
     }
   )
+  
 })
--- ui.R
+++ ui.R
@@ -1,33 +1,138 @@
-# https://github.com/dreamRs/shinyWidgets
+# Title: Shiny Demo App for CRPE Database [UI/Frontend]
+# Owner: Kevin 
+# Date: 6-23-2016
 
-library(shiny)
-library(shinyWidgets)
+#### COMMENT THESE OUT BEFORE PUBLISHING ()
+# rm(list=ls())
+# setwd("/Users/crpe/Documents/Shiny Demo") # MAC
 
+library(shiny) # Need to Run Shiny App
+library(shinyWidgets) # Library of More Widgets like the Dropdown Button (see more at: https://github.com/dreamRs/shinyWidgets)
 
-names <- c('A', 'B', 'C',
-           'D', 'E')
-# Define UI for application that draws a histogram
+## Lists ------------------------------------------------------------------
+# Names for the Options OF Dropdown Menu 1: Datasets
+# Names for the Options OF Dropdown Menu 2: Year
+YEARS <- c('2014-15', '2013-14', '2012-13', 
+           '2011-12', '2010-11', '2009-10', 
+           '2008-09', '2007-08', '2006-07', 
+           '2005-06', '2004-05', '2003-04', 
+           '2002-03', '2001-02', '2000-01' )
+# Names for the Options OF Dropdown Menu 4: City
+STATE <- c('AL', 'AK', 'AZ', 'AR', 'CA', 'CO',
+           'CT', 'DE', 'DC', 'FL', 'GA', 'HI',
+           'ID', 'IL', 'IN', 'IA', 'KS', 'KY',
+           'LA', 'ME', 'MD', 'MA', 'MI', 'MN',
+           'MS', 'MO', 'MT', 'NE', 'NV', 'NH',
+           'NJ', 'NM', 'NY', 'NC', 'ND', 'OH',
+           'OK', 'OR', 'PA', 'RI', 'SC', 'SD',
+           'TN', 'TX', 'UT', 'VT', 'VA', 'WA',
+           'WV', 'WI', 'WY')
+# Names for the Options OF Dropdown Menu 4: City
+CITY <- c('Atlanta', 'Baltimore', 'Boston', 'Camden',
+          'Denver', 'Detroit', 'Houston', 'Indianapolis', 
+          'Kansas City', 'Little Rock', 'Los Angeles', 'Memphis', 
+          'New Orleans', 'New York City', 'Newark', 'Oakland', 
+          'Philadelphia', 'San Antonio', 'Tulsa', 'Washington DC' )
+# Names for the Options OF Dropdown Menu 5: District
+DISTRICT <- c('APS', 'BCPS', 'BPS', 'CSD', 
+              'DPS', 'DPSCD', 'HISD', 'IPS',
+              'KCPS', 'LRSD', 'LAUSD', 'SCS',
+              'OPBD', 'NYC', 'NPS', 'OUSD',
+              'PSD', 'SAISD', 'TPS', 'DCPS')
+
+# Define which fields are mandatory
+fieldsMandatory <- c("dataset", "state")
+
+## Frontend of the Shiny App-----------------------------------------------
 shinyUI(fluidPage(
-  
+  # CRPE Logo
+  img(src='img/crpe.png'),
   # Application title
-  titlePanel("CRPE Demo"),
+  titlePanel("CRPE Database Demo"),
   
-  
+  # SidePanel -------------------------------------------
+  # -The Input/Dropdown Menu that Control the Output
   sidebarLayout(
-    # Dropdown buttons
     sidebarPanel(
+      # Displays Dropdown Menu 1: Datasets
       pickerInput(
         inputId = "dataset", 
         label = "Select Datasets", 
-        choices = LETTERS, options = list(`actions-box` = TRUE), 
+        choices = dbListTables(conn), options = list(`actions-box` = TRUE), 
         multiple = TRUE
-      )
+      ),
+      
+      
+      
+      
+      # Displays Dropdown Menu 2: Year 
+      # pickerInput(
+      #   inputId = "year", 
+      #   label = "Select Year(s)", 
+      #   choices = YEARS, options = list(`actions-box` = TRUE), 
+      #   multiple = TRUE
+      # ),
+      numericInput(
+        inputId = 'year',
+        label = 'Select Which Year',
+        value = '2015'  # Default Starting Value
+      ),
+      
+      # Pick the State
+      pickerInput(
+        inputId = "state", 
+        label = "Select State", 
+        choices = STATE, options = list(`actions-box` = TRUE), 
+        multiple = TRUE
+      ),
+      
+      # Pick the City
+      pickerInput(
+        inputId = "city", 
+        label = "Select City", 
+        choices = CITY, options = list(`actions-box` = TRUE), 
+        multiple = TRUE
+      ),
+      
+      # Pick the District
+      pickerInput(
+        inputId = "district", 
+        label = "Select District", 
+        choices = DISTRICT, options = list(`actions-box` = TRUE), 
+        multiple = TRUE
+      ),
+      
+      # Displays the Submit button
+      actionButton(inputId = "submitBttn", 
+                   label = "Submit",
+                   width = '85px'),
+      
+      # Displays the Reset button
+      actionButton(inputId = "resetBttn", 
+                   label = "Reset",
+                   width = '85px'),
+      
+      # Blank Space
+      hr()
+      
     ),
     
-    # Show a plot of the generated distribution
+    # MainPanel -------------------------------------------
+    # -The Output/Table Displayed Based on Input
     mainPanel(
-      dataTableOutput("my_table"),
-      downloadButton('downloadData', 'Download')
+      # Displays Data Table
+      conditionalPanel(
+        condition = "input.submitBttn == 1",
+        dataTableOutput(outputId = "my_table")
+        # find out if you can include its own scroller
+      ),
+      
+      # Blank Space
+      hr(),
+      
+      # Displays Download Button
+      downloadButton(outputId = 'downloadBttn', 
+                     label = 'Download')
     )
   )
 ))
